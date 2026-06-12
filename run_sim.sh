84885461df8ad623c37f0a727fd2beb78e6d22f7
Iallen Gábio de Sousa Santos <iallen16@gmail.com>
2026-06-12 20:04:14 -0300

    fix(run): fix classpath dependencies in run_sim.sh and enable SimulationMetadata in experiment01

--- run_sim.sh
+++ run_sim.sh
@@ -1,4 +1,2 @@
-CP="/Users/iallen/.m2/repository/com/fasterxml/jackson/core/jackson-databind/2.18.2/jackson-databind-2.18.2.jar:/Users/iallen/.m2/repository/com/fasterxml/jackson/core/jackson-annotations/2.18.2/jackson-annotations-2.18.2.jar:/Users/iallen/.m2/repository/com/fasterxml/jackson/core/jackson-core/2.18.2/jackson-core-2.18.2.jar"
-mkdir -p target/classes
-javac -cp "$CP" -d target/classes src/main/java/com/snets2/**/*.java src/main/java/com/snets2/*.java
-java -cp "target/classes:$CP" com.snets2.MainRunner experiments/experiment01 | grep "RandomFit called" | head -n 10
+#!/bin/bash
+mvn compile exec:java -Dexec.mainClass="com.snets2.MainRunner" -Dexec.args="experiments/experiment01"
